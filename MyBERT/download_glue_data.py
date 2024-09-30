5ded5443723369a14ae84b30480752f2ccc0149d
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-30 17:08:52 +0000

    edit_file
    # Path: [2. Upgrade each .py file]/async_each/for_each/edit_file
    # UUID: aa751aaf-5c9a-4ade-aa6f-1148dd5fbaf0
    
    # Changed files:
    MyBERT/download_glue_data.py
    - 019ce10a739cccfe803797e4ab4271b844174b6a
    - 4cf5b937ca4e7a7778854aa3182305fc391735b4
    - e71fbdcdde63618a05a59c6efc8a4cb5bbcf8af8
    - 3a2a2909359885fca9e7a17f8e82c4b8b91fd253
    - d4f4792ef3390f4808fd070acfdbcf54bf68631c
    - ff5e78a57517b1961231ced144cb478f0bb5de97

--- MyBERT/download_glue_data.py
+++ MyBERT/download_glue_data.py
@@ -20,7 +20,7 @@ import shutil
 import argparse
 import urllib.request
 import zipfile
-from typing import List
+from typing import List, Self
 
 TASKS = ["CoLA", "SST", "MRPC", "QQP", "STS", "MNLI", "SNLI", "QNLI", "RTE", "WNLI", "diagnostic"]
 TASK2PATH = {"CoLA":'https://firebasestorage.googleapis.com/v0/b/mtl-sentence-representations.appspot.com/o/data%2FCoLA.zip?alt=media&token=46d5e637-3411-4188-bc44-5809b5bfb5f4',
