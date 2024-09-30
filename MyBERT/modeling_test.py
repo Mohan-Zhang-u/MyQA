3039e0e288d3d821f9b6ff2c846b535a64f2cf49
Second Agent <123514301+second-agent[bot]@users.noreply.github.com>
2024-09-30 17:08:22 +0000

    edit_file
    # Path: [2. Upgrade each .py file]/async_each/for_each/edit_file
    # UUID: e978f92f-7700-4032-bb10-dcd75d1b3c2d
    
    # Changed files:
    MyBERT/modeling_test.py
    - 188eb73de1db5efe5c7be15207797ce74cda4dfe
    - 37bf6050fdea25845adf2bfd8e8eb012477ae2fc

--- MyBERT/modeling_test.py
+++ MyBERT/modeling_test.py
@@ -20,7 +20,7 @@ import collections
 import json
 import random
 import re
-from typing import TypeVar, Generic, List, Tuple, Dict, Any
+from typing import TypeVar, Generic, List, Tuple, Dict, Any, Self
 
 import modeling
 import six
